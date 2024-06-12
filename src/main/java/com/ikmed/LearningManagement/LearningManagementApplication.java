46f0555a7bac856d1e527a7cc0d479ce6ad23e1a
Daniel Silva De Lima <daniel_lma@hotmail.com>
2024-06-11 22:38:56 -0300

    Arquivos complementares

---
+++ src/main/java/com/ikmed/LearningManagement/LearningManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ikmed.LearningManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningManagementApplication.class, args);
+	}
+
+}
